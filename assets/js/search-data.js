cc573a6a9289c6fd6b092e27d8494d397774e295
thiagorr162 <30710465+thiagorr162@users.noreply.github.com>
2025-10-06 13:21:02 +0000

    Deploying to gh-pages from @ thiagorr162/thiagorr162.github.io@1059f1cd58d642a076f8f492041c682ef599b4a5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-notes",
           title: "notes",
-          description: "",
+          description: "Lecture notes",
           section: "Navigation",
           handler: () => {
             window.location.href = "/notes/";
